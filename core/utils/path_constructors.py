9247f48311c737054a7c95ac9de2a621b84a8576
konstantin <kmerezhnikov2002@yandex.ru>
2022-11-12 20:48:05 +0300

    Migrations

--- core/utils/path_constructors.py
+++ core/utils/path_constructors.py
@@ -1,9 +1,6 @@
-from core.models import Sticker, Widget
-
-
-def path_to_widget(instance: Widget, file) -> str:
+def path_to_widget(instance, file) -> str:
     return f'widgets/{instance.pk}/{file}'
 
 
-def path_to_sticker(instance: Sticker, file) -> str:
+def path_to_sticker(instance, file) -> str:
     return f'stickers/{instance.sticker_pack.name}/{file}'

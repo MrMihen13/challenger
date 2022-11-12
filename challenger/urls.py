dff9f833b217e008d7915fc0e31de19e5f2b7a77
konstantin <kmerezhnikov2002@yandex.ru>
2022-11-13 02:45:59 +0300

    Deploy

--- challenger/urls.py
+++ challenger/urls.py
@@ -1,11 +1,12 @@
 """challenger URL Configuration"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 from challenger.api_docs import api_v1
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_v1)),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
